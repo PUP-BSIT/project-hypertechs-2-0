5982071ef42762f51f958a13b74cefdd94911ac5
mfckinstarboy <emmanuellmartinez013@gmail.com>
2024-05-29 17:19:15 +0800

    add login routing

--- scribe-app/src/app/app-routing.module.ts
+++ scribe-app/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingComponent } from './public/landing/landing.component';
+import { LoginComponent } from './auth/login/login.component';
 
 const routes: Routes = [
   {path: '', component: LandingComponent}, // default path
+  {path: 'login', component: LoginComponent} // landing -> login
 ];
 
 @NgModule({
