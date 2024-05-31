08d853bfedeb6f3d3b4d29e40c567d32eeb767c4
danay_maryjoy <maryjoydanay2.0@gmail.com>
2024-05-31 18:13:51 +0800

    import loginservice into component

--- scribe/src/app/auth/login/login.component.ts
+++ scribe/src/app/auth/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 import { LoginData } from '../../../models/model';
+import { LoginService } from '../../../services/login.service';
 
 @Component({
   selector: 'app-login',
