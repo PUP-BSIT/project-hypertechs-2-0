173f2d76bbb6586e40d2865965ddfcc92e7af8ee
danay_maryjoy <maryjoydanay2.0@gmail.com>
2024-05-31 18:11:53 +0800

    import fomrbuilder

--- scribe/src/app/auth/login/login.component.ts
+++ scribe/src/app/auth/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, Validators } from '@angular/forms';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
