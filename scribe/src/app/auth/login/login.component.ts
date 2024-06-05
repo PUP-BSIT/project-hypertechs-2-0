400fc310387b095d9f936288a43e8761dd2927f0
danay_maryjoy <maryjoydanay2.0@gmail.com>
2024-06-06 04:29:31 +0800

    remove unnecessary component formcontrol

--- scribe/src/app/auth/login/login.component.ts
+++ scribe/src/app/auth/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 import { LoginData } from '../../../models/model';
 import { LoginService } from '../../../services/login.service';
 import { Router } from '@angular/router';
