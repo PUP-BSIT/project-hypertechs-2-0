6a2d56f6f49b63900f041a21133a9e3f31902ebf
danay_maryjoy <maryjoydanay2.0@gmail.com>
2024-06-06 04:27:49 +0800

    refractor error message using property

--- scribe/src/app/auth/login/login.component.ts
+++ scribe/src/app/auth/login/login.component.ts
@@ -71,10 +71,35 @@ export class LoginComponent implements OnInit{
         this.router.navigate(['main/home'], { queryParams: { username: response.username} });
         //alert(`Log In Successful! Hi ${response.username}`);
       },
-      error:(error: any)=>{
-        console.error("Error Object", error);
-        this.errorMessage = 'Login failed.'; }
-      });
-      
+      error:(error: HttpErrorResponse)=>{
+        this.handleError(error) },
+      }); 
   } 
+
+  handleError(error: HttpErrorResponse){
+    this.errorMessage = 'Login failed';
+  
+    if (error.error) {
+      this.errorMessage = error.error.error; 
+    }
+
+    if (error?.status){
+      switch (error.status) {
+        case 400:
+          this.errorMessage = 'Bad request. Please check your data.';
+          break;
+        case 401:
+          this.errorMessage = 
+            'You have entered an invalid email or password.';
+          break;
+        case 500:
+          this.errorMessage = 
+            'Internal server error. Please try again later.';
+          break;
+        default:
+          this.errorMessage = 
+          `Error: ${error.status}. Please try again later.`;
+      } 
+    } 
+  }      
 }
