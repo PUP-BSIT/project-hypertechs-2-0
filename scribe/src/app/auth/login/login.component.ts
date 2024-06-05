d4bd30317913f2ec46c97390ddda3b1099b60a89
danay_maryjoy <maryjoydanay2.0@gmail.com>
2024-06-06 04:01:36 +0800

    import httperror response

--- scribe/src/app/auth/login/login.component.ts
+++ scribe/src/app/auth/login/login.component.ts
@@ -3,6 +3,7 @@ import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms'
 import { LoginData } from '../../../models/model';
 import { LoginService } from '../../../services/login.service';
 import { Router } from '@angular/router';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
