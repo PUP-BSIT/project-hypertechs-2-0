0c4240db822757d757a400da487b8a631074a833
danay_maryjoy <maryjoydanay2.0@gmail.com>
2024-06-06 05:32:11 +0800

    declare necessary values as required thru validators

--- scribe/src/app/auth/signup/signup.component.ts
+++ scribe/src/app/auth/signup/signup.component.ts
@@ -9,11 +9,18 @@ import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 export class SignupComponent implements OnInit{
 
   constructor(private formBuilder: FormBuilder){}
-  
+
   signupForm: FormGroup = this.formBuilder.group({});
 
   ngOnInit(): void {
     
+    this.signupForm = this.formBuilder.group({
+      lastname:['', Validators.required],
+      firstname:['', Validators.required],
+      email:['', [Validators.required, Validators.email]],
+      password:['', Validators.required],
+      confirm_password:['', Validators.required]
+    })
   }
 
 }
