cbdb24daf04f6656a42c560baa0919e74f6da5b8
danay_maryjoy <maryjoydanay2.0@gmail.com>
2024-06-06 10:37:20 +0800

    define php headers

---
+++ scribe/src/backend/signup.php
@@ -0,0 +1,7 @@
+<?php
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: POST, GET, OPTIONS'); 
+    header('Access-Control-Allow-Headers: Content-Type');
+    header('Content-Type: application/json; charset=utf-8'); // Set content type to JSON
+    
+?>
