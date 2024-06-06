45022ff9cedd0c5ff250207f0856813e54d31654
danay_maryjoy <maryjoydanay2.0@gmail.com>
2024-06-06 10:41:37 +0800

    check request method in database

--- scribe/src/backend/signup.php
+++ scribe/src/backend/signup.php
@@ -17,5 +17,10 @@
         die("Connection failed: " . $conn->connect_error);
     }
 
+    if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+        http_response_code(200);
+        exit();
+    }
+    
     
 ?>
