c9825e2c9b5b9ea59d22c438b46d75bc4cc31204
Nabayra, James V. <146575214+nabayra-james@users.noreply.github.com>
2024-06-07 17:09:55 +0800

    modify enter-new-password components

--- scribe/src/app/auth/recovery/enter-new-password/enter-new-password.component.ts
+++ scribe/src/app/auth/recovery/enter-new-password/enter-new-password.component.ts
@@ -3,13 +3,38 @@ import { Component, EventEmitter, Output } from '@angular/core';
 @Component({
   selector: 'app-enter-new-password',
   templateUrl: './enter-new-password.component.html',
-  styleUrls: ['./enter-new-password.component.scss']
+  styleUrls: ['./enter-new-password.component.scss', '../recovery.component.scss']
 })
 export class EnterNewPasswordComponent {
   newPassword: string = '';
   @Output() passwordSubmitted = new EventEmitter<string>();
+  showSuccessModal: boolean = false;
+
+  ngOnInit() {
+    this.openModal();
+  }
 
   submitPassword() {
-    this.passwordSubmitted.emit(this.newPassword);
+    console.log('Password reset successfully');
+
+    this.showSuccessModal = true;
+  }
+
+  openModal() {
+    const modal = document.getElementById('password_modal');
+    if (modal) {
+      modal.style.display = 'block';
+    }
+  }
+
+  closeModal() {
+    const modal = document.getElementById('password_modal');
+    if (modal) {
+      modal.style.display = 'none';
+    }
+  }
+
+  closeSuccessModal() {
+    this.showSuccessModal = false;
   }
 }
