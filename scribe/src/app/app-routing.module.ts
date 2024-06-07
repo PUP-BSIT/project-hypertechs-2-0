4d2c1a8a22c90ce5a74bdacc48acfa891cf53425
danay_maryjoy <maryjoydanay2.0@gmail.com>
2024-06-08 06:43:05 +0800

    import auth guard to routing

--- scribe/src/app/app-routing.module.ts
+++ scribe/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from '../guards/auth.guard';
 
 /* Scribe App Components*/
 import { LandingComponent } from './public/landing/landing.component';
