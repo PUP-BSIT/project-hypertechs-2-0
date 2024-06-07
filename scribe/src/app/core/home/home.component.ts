94e575e9823cd617f415dfd5455316e6c9f7e7ac
danay_maryjoy <maryjoydanay2.0@gmail.com>
2024-06-08 07:04:32 +0800

    remove unnecessary function

--- scribe/src/app/core/home/home.component.ts
+++ scribe/src/app/core/home/home.component.ts
@@ -9,18 +9,9 @@ import { Router } from '@angular/router';
 })
 
 export class HomeComponent{
-  //firstname: string = '';
+  
   @Input() firstname: string | null = null;
 
   constructor(private route: ActivatedRoute, private router: Router) { }
 
-  // ngOnInit() {
-  //   //this.firstname = this.route.snapshot.queryParams['firstname'];
-    
-  // }
-  
-  logout() {
-    sessionStorage.removeItem('loggedInUser');
-    this.router.navigate(['/login']); // Redirect to login page
-}
 }
