ca04c18703d43a5b9cf7e937e29fa2de65bd0e1a
mfckinstarboy <emmanuellmartinez013@gmail.com>
2024-06-09 17:52:26 +0800

    fix wrong database name

--- scribe/src/backend/auth/login.php
+++ scribe/src/backend/auth/login.php
@@ -9,7 +9,7 @@
     $servername = "localhost";
     $username = "root";
     $password = "";
-    $dbname = "scribe_db";
+    $dbname = "test_scribe_db";
 
     // Create connection
     $conn = new mysqli($servername, $username, $password, $dbname);
