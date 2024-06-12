c2c41c062a21df22467b508b331fb394278bbe4f
danay_maryjoy <maryjoydanay2.0@gmail.com>
2024-06-12 22:35:46 +0800

    remove unnecessary comment

--- scribe/src/backend/auth/config.php
+++ scribe/src/backend/auth/config.php
@@ -2,8 +2,9 @@
 $expire = 864000; // One year duration (adjust as needed)
 
 // Set session garbage collection
-ini_set('session.cookie_lifetime', $expire); 
+ini_set('session.cookie_lifetime', $expire);
 
-// Start session (optional, can be called in other files)
-session_start(); 
+// Start session
+session_start();
+
 ?>
