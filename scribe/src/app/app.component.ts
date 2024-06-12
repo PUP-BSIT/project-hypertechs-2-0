6a0389d90d9e4cbca6a225e4388361c2a110b12e
mfckinstarboy <emmanuellmartinez013@gmail.com>
2024-06-12 09:05:34 +0800

    update services directory in app root

--- scribe/src/app/app.component.ts
+++ scribe/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, Renderer2 } from '@angular/core';
-import { ThemeService } from '../services/theme.service'
+import { ThemeService } from '../services/theme/theme.service';
 import { RouterOutlet } from '@angular/router';
 import { RouteTransitionAnimations } from '../animations/route-animations';
 
@@ -7,14 +7,13 @@ import { RouteTransitionAnimations } from '../animations/route-animations';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  animations: [RouteTransitionAnimations]
+  animations: [RouteTransitionAnimations],
 })
 export class AppComponent {
   title = 'scribe';
 
   constructor(private themeService: ThemeService, private renderer: Renderer2) {
-
-    this.themeService.currentTheme.subscribe(isDarkMode => {
+    this.themeService.currentTheme.subscribe((isDarkMode) => {
       if (isDarkMode) {
         this.renderer.addClass(document.body, 'dark-mode');
       } else {
@@ -31,8 +30,10 @@ export class AppComponent {
 
   /* Add route transitions to the root component */
   prepareRoute(outlet: RouterOutlet) {
-    return outlet && 
-      outlet.activatedRouteData && 
-      outlet.activatedRouteData['animationState'];
-   }
+    return (
+      outlet &&
+      outlet.activatedRouteData &&
+      outlet.activatedRouteData['animationState']
+    );
+  }
 }
