7b58eb2f2ea88a4f4e3430aec4cff7e9465371cd
mfckinstarboy <emmanuellmartinez013@gmail.com>
2024-06-13 19:37:52 +0800

    discard note data placeholder

--- scribe/src/app/core/home/home.component.ts
+++ scribe/src/app/core/home/home.component.ts
@@ -3,7 +3,6 @@ import { ActivatedRoute } from '@angular/router';
 import { Router } from '@angular/router';
 import { UserService } from '../../../services/user/user.service';
 import { TitleCaseService } from '../../../services/title-case/title-case.service';
-import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 
 interface Template {
   icon: string;
@@ -40,55 +39,4 @@ export class HomeComponent implements OnInit {
     { icon: 'school', title: 'Lectures' },
     { icon: 'today', title: 'Daily Planner' },
   ];
-
-  notes = [
-    {
-      id: 1,
-      title: 'Note 1',
-      content: 'This is a sample note 1.',
-      lastEdited: new Date(),
-    },
-    {
-      id: 2,
-      title: 'Note 2',
-      content: 'This is a sample note 2',
-      lastEdited: new Date(),
-    },
-    {
-      id: 3,
-      title: 'Note 3',
-      content: 'This is a sample note 3',
-      lastEdited: new Date(),
-    },
-    {
-      id: 1,
-      title: 'Note 4',
-      content: 'This is a sample note 1',
-      lastEdited: new Date(),
-    },
-    {
-      id: 2,
-      title: 'Note 5',
-      content: 'This is a sample note 2',
-      lastEdited: new Date(),
-    },
-    {
-      id: 3,
-      title: 'Note 6',
-      content: 'This is a sample note 3',
-      lastEdited: new Date(),
-    },
-  ];
-
-  drop(event: CdkDragDrop<string[]>) {
-    moveItemInArray(this.notes, event.previousIndex, event.currentIndex);
-  }
-
-  createNote() {
-    console.log('Create Note button clicked');
-  }
-
-  useTemplate(template: Template) {
-    console.log(`Using template: ${template.title}`);
-  }
 }
