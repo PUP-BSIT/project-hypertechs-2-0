9539bc6ef93e08da9ba3a2dceb7b4160025af7b5
mfckinstarboy <emmanuellmartinez013@gmail.com>
2024-06-18 22:01:28 +0800

    load notes from the database

--- scribe/src/app/core/notes/notes.component.ts
+++ scribe/src/app/core/notes/notes.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NoteService } from '../../../services/notes/note.service';
 
 @Component({
   selector: 'app-notes',
   templateUrl: './notes.component.html',
-  styleUrl: './notes.component.scss'
+  styleUrls: ['./notes.component.scss'],
 })
-export class NotesComponent {
+export class NotesComponent implements OnInit {
+  notes: any[] = [];
 
+  constructor(private noteService: NoteService) {}
+
+  ngOnInit() {
+    this.loadNotes();
+  }
+
+  loadNotes() {
+    this.noteService.getNotes().subscribe(
+      (data) => {
+        console.log('Notes received from backend:', data);
+        this.notes = data;
+      },
+      (error) => {
+        console.error('Error fetching notes:', error);
+      }
+    );
+  }
 }
