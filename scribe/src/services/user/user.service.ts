5796fff80dfb34d94e021fa0bd4ab975ed8b1703
mfckinstarboy <emmanuellmartinez013@gmail.com>
2024-06-20 15:32:48 +0800

    add user_id on user service

--- scribe/src/services/user/user.service.ts
+++ scribe/src/services/user/user.service.ts
@@ -2,17 +2,19 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root' // Make the service available throughout the application
+  providedIn: 'root'
 })
 export class UserService {
 
   private firstnameSubject = new BehaviorSubject<string | null>(null);
   private lastnameSubject = new BehaviorSubject<string | null>(null);
   private emailSubject = new BehaviorSubject<string | null>(null);
+  private userIdSubject = new BehaviorSubject<number | null>(null);
 
   firstname$ = this.firstnameSubject.asObservable();
   lastname$ = this.lastnameSubject.asObservable();
   email$ = this.emailSubject.asObservable();
+  userId$ = this.userIdSubject.asObservable();
 
   constructor() { }
 
@@ -40,10 +42,21 @@ export class UserService {
     return this.emailSubject.getValue();
   }
 
-  isLoggedIn(): boolean {
-    // Replace this with your actual login state checking logic
-    const loggedInUser = localStorage.getItem('loggedInUser');
-    return loggedInUser !== null; // Check if a logged-in user exists in session storage
+  setUserId(userId: number) {
+    this.userIdSubject.next(userId);
+    localStorage.setItem('user_id', userId.toString());
   }
 
+  getUserId(): number | null {
+    if (this.userIdSubject.getValue() === null) {
+      const storedUserId = localStorage.getItem('user_id');
+      this.userIdSubject.next(storedUserId ? parseInt(storedUserId, 10) : null);
+    }
+    return this.userIdSubject.getValue();
+  }
+
+  isLoggedIn(): boolean {
+    const loggedInUser = localStorage.getItem('loggedInUser');
+    return loggedInUser !== null;
+  }
 }
