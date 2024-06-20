e8abdd41784368a20151952ad3afc4bf2d87ca6a
mfckinstarboy <emmanuellmartinez013@gmail.com>
2024-06-20 15:33:09 +0800

    update notes APIs

--- scribe/src/services/notes/note.service.ts
+++ scribe/src/services/notes/note.service.ts
@@ -1,29 +1,40 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class NoteService {
-
   private apiUrl = 'http://localhost/backend/notes';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private authService: AuthService) {}
 
   getNotes(): Observable<any> {
-    return this.http.get(`${this.apiUrl}/get-notes.php`);
+    const userId = this.authService.getUserId();
+    return this.http.get(`${this.apiUrl}/get-notes.php?user_id=${userId}`);
   }
 
   saveNote(noteData: any): Observable<any> {
+    const userId = this.authService.getUserId();
+    noteData.user_id = userId;
     return this.http.post(`${this.apiUrl}/save-notes.php`, noteData);
   }
 
   updateNote(noteId: number, noteData: any): Observable<any> {
-    return this.http.post(`${this.apiUrl}/update-notes.php?id=${noteId}`, noteData);
+    const userId = this.authService.getUserId();
+    noteData.user_id = userId;
+    return this.http.post(
+      `${this.apiUrl}/update-notes.php?id=${noteId}`,
+      noteData
+    );
   }
-  
+
   getNoteById(noteId: number): Observable<any> {
-    return this.http.get(`${this.apiUrl}/get-notes-id.php?id=${noteId}`);
+    const userId = this.authService.getUserId();
+    return this.http.get(
+      `${this.apiUrl}/get-notes-id.php?id=${noteId}&user_id=${userId}`
+    );
   }
 }
