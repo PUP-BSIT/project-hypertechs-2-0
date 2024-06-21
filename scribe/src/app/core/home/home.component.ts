3b6660e2e39f2c2d97359c3939103c93c72774e9
mfckinstarboy <emmanuellmartinez013@gmail.com>
2024-06-21 11:06:30 +0800

    display recent notes on home

--- scribe/src/app/core/home/home.component.ts
+++ scribe/src/app/core/home/home.component.ts
@@ -1,29 +1,62 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { UserService } from '../../../services/user/user.service';
 import { TitleCaseService } from '../../../services/title-case/title-case.service';
 import { FeaturedTemplates } from '../../../models/model';
+import { NoteService } from '../../../services/notes/note.service';
+import { AuthService } from '../../../services/auth/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
   @Input() firstname: string | null = null;
   @Input() note: any;
+  notes: any[] = [];
+  private userSubscription!: Subscription;
 
   constructor(
     private route: ActivatedRoute,
     private router: Router,
     private userService: UserService,
-    private titleCaseService: TitleCaseService
+    private titleCaseService: TitleCaseService,
+    private noteService: NoteService,
+    private authService: AuthService
   ) {}
 
   ngOnInit() {
     this.userService.firstname$.subscribe((firstname) => {
       this.firstname = this.titleCaseService.toTitleCase(firstname);
     });
+
+    this.userSubscription = this.authService.user$.subscribe((userId) => {
+      if (userId !== null) {
+        this.loadNotes();
+      } else {
+        this.notes = []; // Clear notes when no user is logged in
+      }
+    });
+  }
+
+  loadNotes() {
+    this.noteService.getNotes().subscribe(
+      (data) => {
+        console.log('Notes received from backend:', data);
+        this.notes = data;
+      },
+      (error) => {
+        console.error('Error fetching notes:', error);
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    if (this.userSubscription) {
+      this.userSubscription.unsubscribe();
+    }
   }
 
   templates: FeaturedTemplates[] = [
