ed2cc8d18d45b6ee8da5b4e499dcc45bfb90be7b
mfckinstarboy <emmanuellmartinez013@gmail.com>
2024-06-23 09:28:18 +0800

    filter deleted notes

--- scribe/src/app/core/notes/notes.component.ts
+++ scribe/src/app/core/notes/notes.component.ts
@@ -14,33 +14,40 @@ export class NotesComponent implements OnInit, OnDestroy {
   private userSubscription!: Subscription;
 
   constructor(
-    private noteService: NoteService, 
+    private noteService: NoteService,
     private authService: AuthService
   ) {}
 
   ngOnInit() {
-    this.userSubscription = this.authService.user$.subscribe(userId => {
+    this.userSubscription = this.authService.user$.subscribe((userId) => {
+      console.log('User ID:', userId);
       if (userId !== null) {
         this.loadNotes();
       } else {
-        this.notes = []; // Clear notes when no user is logged in
-        this.isLoading = false; // Set loading to false if no user
+        this.notes = [];
+        this.isLoading = false;
       }
     });
   }
 
   loadNotes() {
+    console.log('Loading notes...');
     this.noteService.getNotes().subscribe(
-      (data) => {
+      (data: any[]) => {
         console.log('Notes received from backend:', data);
-        this.notes = data;
-        this.isLoading = false; // Set the flag to false when loading is complete
+        this.notes = data.filter((note) => note.is_deleted == 0);
+        console.log('Filtered notes:', this.notes);
+        this.isLoading = false;
       },
       (error) => {
         console.error('Error fetching notes:', error);
-        this.isLoading = false; // Set the flag to false even on error
+        this.isLoading = false;
       }
     );
+  }
+
+  onNoteDelete(noteId: number) {
+    this.notes = this.notes.filter((note) => note.id !== noteId);
   }
 
   ngOnDestroy() {
