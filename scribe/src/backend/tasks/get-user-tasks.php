a624a411e21b29aebb548c12a05e7cd23591b44c
mfckinstarboy <emmanuellmartinez013@gmail.com>
2024-06-27 11:28:20 +0800

    add get-user API for editing

---
+++ scribe/src/backend/tasks/get-user-tasks.php
@@ -0,0 +1,31 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type');
+header('Content-Type: application/json; charset=utf-8');
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "scribe_db";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$user_id = $_GET['user_id'];
+
+$sql = "SELECT * FROM tasks WHERE user_id = ?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("i", $user_id);
+$stmt->execute();
+$result = $stmt->get_result();
+
+$tasks = array();
+while ($row = $result->fetch_assoc()) {
+    $tasks[] = $row;
+}
+
+echo json_encode($tasks);
