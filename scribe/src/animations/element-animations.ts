f9ba5f2d8e39898b295a10a4793bd5ea9a4b6d6d
mfckinstarboy <emmanuellmartinez013@gmail.com>
2024-06-27 20:57:45 +0800

    add half slide animation

--- scribe/src/animations/element-animations.ts
+++ scribe/src/animations/element-animations.ts
@@ -64,16 +64,34 @@ export const slideUpDown = trigger('slideUpDown', [
 /* Animation 4: Task Animation */
 export const taskAnimation = trigger('taskAnimation', [
   transition(':enter', [
-    style({ opacity: 0, transform: 'translateY(25%)' }),
+    style({ opacity: 0, transform: 'translateY(45%)' }),
     animate(
-      '500ms cubic-bezier(.09,.01,.22,.99)',
+      '600ms cubic-bezier(.09,.01,.22,.99)',
       style({ opacity: 1, transform: 'translateY(0)' })
     )
   ]),
   transition(':leave', [
     animate(
-      '500ms cubic-bezier(0.25, 0.8, 0.25, 1)',
+      '600ms cubic-bezier(0.25, 0.8, 0.25, 1)',
       style({ opacity: 0, transform: 'translateX(-50%)' })
     )
   ])
+]);
+
+
+/* Animation 5: half Slide Animation */
+export const halfSlide = trigger('halfSlide', [
+  transition(':enter', [
+    style({ transform: 'translateY(20%)', opacity: 0 }),
+    animate(
+      '800ms cubic-bezier(0.25, 0.8, 0.25, 1)',
+      style({ transform: 'translateY(0)', opacity: 1 })
+    ),
+  ]),
+  transition(':leave', [
+    animate(
+      '800ms cubic-bezier(0.25, 0.8, 0.25, 1)',
+      style({ transform: 'translateY(100%)', opacity: 0 })
+    ),
+  ]),
 ]);
