e292f095610968e57f3afa5850b190518db4673c
mfckinstarboy <emmanuellmartinez013@gmail.com>
2024-06-27 11:26:45 +0800

    display tasks to card

--- scribe/src/app/core/components/task-card/task-card.component.ts
+++ scribe/src/app/core/components/task-card/task-card.component.ts
@@ -1,10 +1,33 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-task-card',
   templateUrl: './task-card.component.html',
-  styleUrls: ['./task-card.component.scss'],
+  styleUrls: ['./task-card.component.scss']
 })
-export class TaskCardComponent {
-  @Input() task!: { title: string; description: string; progress: number };
+export class TaskCardComponent implements OnInit {
+  @Input() task: any;
+
+  todoCount: number = 0;
+  inProgressCount: number = 0;
+  doneCount: number = 0;
+  progress: number = 0;
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.todoCount = JSON.parse(this.task.todo).length;
+    this.inProgressCount = JSON.parse(this.task.in_progress).length;
+    this.doneCount = JSON.parse(this.task.done).length;
+    const totalCount = this.todoCount + this.inProgressCount + this.doneCount;
+    this.progress = totalCount === 0 ? 0 : (this.doneCount / totalCount) * 100;
+  }
+
+  openTaskBoard() {
+    console.log('Task object:', this.task);
+    console.log('Task ID:', this.task.task_id);
+    this.router.navigate(['/main/board', { taskId: this.task.task_id }]);
+  }
+  
 }
