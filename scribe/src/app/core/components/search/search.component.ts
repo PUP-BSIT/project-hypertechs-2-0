8e3078adbe53ed977a6a97434e512d1b8fa50829
danay_maryjoy <maryjoydanay2.0@gmail.com>
2024-06-28 22:41:35 +0800

    import necessary module for search

--- scribe/src/app/core/components/search/search.component.ts
+++ scribe/src/app/core/components/search/search.component.ts
@@ -1,6 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { simpleFade, slideInOut } from '../../../../animations/element-animations';
-
+import { SearchService } from '../../../../services/search/search.service';
+import { EventEmitter, Output, Input } from '@angular/core';
+import { NoteService } from '../../../../services/notes/note.service';
+import { Subscription, switchMap } from 'rxjs';
+import { AuthService } from '../../../../services/auth/auth.service';
 
 @Component({
   selector: 'app-search',
