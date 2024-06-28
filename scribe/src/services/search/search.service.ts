435d955262f0b8dc67ed2ae870a8b9ba77503b41
danay_maryjoy <maryjoydanay2.0@gmail.com>
2024-06-28 21:28:19 +0800

    import necessary modules

--- scribe/src/services/search/search.service.ts
+++ scribe/src/services/search/search.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchService {
 
-  constructor() { }
+  
 }
