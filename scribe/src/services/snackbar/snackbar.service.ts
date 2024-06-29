a88845a5f53794c561fbb75e22bff17db34aa318
mfckinstarboy <emmanuellmartinez013@gmail.com>
2024-06-29 21:34:23 +0800

    add an optional callback for action

--- scribe/src/services/snackbar/snackbar.service.ts
+++ scribe/src/services/snackbar/snackbar.service.ts
@@ -1,19 +1,32 @@
 import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatSnackBarRef, SimpleSnackBar } from '@angular/material/snack-bar';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SnackbarService {
+  constructor(private snackBar: MatSnackBar) {}
 
-  constructor(private snackBar: MatSnackBar) { }
+  show(
+    message: string,
+    action: string = 'Close',
+    duration: number = 5000,
+    actionCallback?: () => void
+  ) {
+    let snackBarRef: MatSnackBarRef<SimpleSnackBar>;
 
-  show(message: string, action: string = 'Close', duration: number = 5000) {
-    this.snackBar.open(message, action, {
+    snackBarRef = this.snackBar.open(message, action, {
       duration: duration,
       verticalPosition: 'bottom',
       horizontalPosition: 'center',
     });
+
+    if (actionCallback) {
+      snackBarRef.onAction().subscribe(() => {
+        actionCallback();
+      });
+    }
   }
 
   dismiss() {
