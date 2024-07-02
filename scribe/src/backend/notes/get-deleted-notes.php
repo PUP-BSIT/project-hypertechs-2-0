e19e49d6f4b493142488c914243e0c37751d00e9
mfckinstarboy <emmanuellmartinez013@gmail.com>
2024-07-02 21:08:30 +0800

    add sorting parameter to API

--- scribe/src/backend/notes/get-deleted-notes.php
+++ scribe/src/backend/notes/get-deleted-notes.php
@@ -17,14 +17,32 @@ if ($conn->connect_error) {
 }
 
 $user_id = intval($_GET['user_id']);
+$sort_by = isset($_GET['sort_by']) ? $_GET['sort_by'] : 'lastEdited';
 
-$sql = "SELECT * FROM notes WHERE user_id = $user_id AND is_deleted = 1";
+switch ($sort_by) {
+    case 'dateCreated':
+        $order_by = 'last_edited ASC';
+        break;
+    case 'titleAsc':
+        $order_by = 'title ASC';
+        break;
+    case 'titleDesc':
+        $order_by = 'title DESC';
+        break;
+    case 'lastEdited':
+    default:
+        $order_by = 'last_edited DESC';
+        break;
+}
+
+$sql = "SELECT * FROM notes WHERE user_id = $user_id AND 
+    is_deleted = 1 ORDER BY $order_by";
 
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     $notes = [];
-    while($row = $result->fetch_assoc()) {
+    while ($row = $result->fetch_assoc()) {
         $notes[] = $row;
     }
     echo json_encode($notes);
