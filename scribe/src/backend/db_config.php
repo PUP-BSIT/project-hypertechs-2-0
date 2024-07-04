0d6f047c0a6f4fb9011003913f00d4aeca4f9053
mfckinstarboy <emmanuellmartinez013@gmail.com>
2024-07-04 12:12:26 +0800

    add global database configuration

---
+++ scribe/src/backend/db_config.php
@@ -0,0 +1,17 @@
+<?php
+
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type');
+header('Content-Type: application/json; charset=utf-8');
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "scribe_db";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
