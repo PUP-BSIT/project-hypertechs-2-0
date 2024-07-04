1502fb67a83dea5c95409c152335e2d52c7ca4b4
mfckinstarboy <emmanuellmartinez013@gmail.com>
2024-07-04 14:29:09 +0800

    include db config in search API endpoint

--- scribe/src/backend/search/search.php
+++ scribe/src/backend/search/search.php
@@ -1,23 +1,5 @@
 <?php
-// Allow CORS
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type');
-header('Content-Type: application/json; charset=utf-8');
-
-// Database connection credentials
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "scribe_db";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+include '../db_config.php';
 
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     http_response_code(200);
@@ -68,4 +50,3 @@ echo json_encode($combined_results);
 $notes_stmt->close();
 $tasks_stmt->close();
 $conn->close();
-?>
