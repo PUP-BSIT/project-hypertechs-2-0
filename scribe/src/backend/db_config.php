5bd40d0b4a31b91d363c05fe70fbe8bc7cfef97a
casabuenangel <casabuenaangelrose1123@gmail.com>
2024-07-05 19:54:50 +0800

    change url for deployment

--- scribe/src/backend/db_config.php
+++ scribe/src/backend/db_config.php
@@ -5,10 +5,10 @@ header('Access-Control-Allow-Methods: POST, PUT, GET, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type');
 header('Content-Type: application/json; charset=utf-8');
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "scribe_db";
+$servername = "127.0.0.1:3306";
+$username = "u565642650_scribe_user";
+$password = "Hypertechs2.0_dbpass";
+$dbname = "u565642650_scribe_db";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
--- scribe/src/services/search/search.service.ts
+++ scribe/src/services/search/search.service.ts
@@ -9,7 +9,7 @@ import { catchError } from 'rxjs/operators';
 })
 export class SearchService {
 
-  private apiUrl = 'http://localhost/backend/search/search.php';
+  private apiUrl = 'https://preview.scribenote.tech/backend/search/search.php';
   private notesSubject = new BehaviorSubject<any[]>([]);
   private searchTermSubject = new BehaviorSubject<string>('');
 
