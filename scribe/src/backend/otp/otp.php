1df8ca058ded3769e183702f80ab7a281185e1bd
mfckinstarboy <emmanuellmartinez013@gmail.com>
2024-07-05 18:24:55 +0800

    fix command sync error in signup

--- scribe/src/backend/otp/otp.php
+++ scribe/src/backend/otp/otp.php
@@ -62,7 +62,6 @@ if (isset($data['otp'])) {
             );
             $updateStmt->bind_param("i", $user_id);
             $updateStmt->execute();
-            $updateStmt->fetch();
             $updateStmt->close();
 
             $selectStmt = $conn->prepare(
