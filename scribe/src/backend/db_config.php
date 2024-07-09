636469beb8944d979620b6cc2ba2912c8fd3782f
danay_maryjoy <maryjoydanay2.0@gmail.com>
2024-07-09 22:51:47 +0800

    change url

--- scribe/src/backend/db_config.php
+++ scribe/src/backend/db_config.php
@@ -5,14 +5,13 @@ header('Access-Control-Allow-Methods: POST, PUT, GET, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type');
 header('Content-Type: application/json; charset=utf-8');
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "scribe_db";
+$servername = "127.0.0.1:3306";
+$username = "u565642650_scribe_user";
+$password = "Hypertechs2.0_dbpass";
+$dbname = "u565642650_scribe_db";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
-    //test
     die("Connection failed: " . $conn->connect_error);
 }
