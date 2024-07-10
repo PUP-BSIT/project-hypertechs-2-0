90becd7fb11ec6cb8e23ae49f90e917a35488b3f
mfckinstarboy <emmanuellmartinez013@gmail.com>
2024-07-10 22:40:04 +0800

    enhance validation and error messages

--- scribe/src/app/core/components/lock-dialog/lock-dialog.component.ts
+++ scribe/src/app/core/components/lock-dialog/lock-dialog.component.ts
@@ -1,24 +1,61 @@
 import { Component, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NoteService } from '../../../../services/notes/note.service';
 
 @Component({
   selector: 'app-lock-dialog',
   templateUrl: './lock-dialog.component.html',
-  styleUrls: ['./lock-dialog.component.scss']
+  styleUrls: ['./lock-dialog.component.scss'],
 })
 export class LockDialogComponent {
-  password: string = '';
+  passwordForm: FormGroup;
 
   constructor(
     public dialogRef: MatDialogRef<LockDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any
-  ) {}
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private formBuilder: FormBuilder,
+    private noteService: NoteService
+  ) {
+    this.passwordForm = this.formBuilder.group({
+      password: ['', [Validators.required, Validators.pattern(/^\d{6}$/)]],
+    });
+  }
 
   onNoClick(): void {
     this.dialogRef.close();
   }
 
   onConfirmClick(): void {
-    this.dialogRef.close({ password: this.password });
+    if (this.passwordForm.valid) {
+      const password = this.passwordForm.get('password')?.value;
+
+      if (this.data.noteId) {
+        // Check password if noteId is provided (for unlocking)
+        this.noteService
+          .checkNotePassword(this.data.noteId, password)
+          .subscribe(
+            (isCorrect) => {
+              if (isCorrect) {
+                this.dialogRef.close({ password: password });
+              } else {
+                this.passwordForm
+                  .get('password')
+                  ?.setErrors({ incorrectPassword: true });
+              }
+            },
+            (error) => {
+              console.error('Error checking password:', error);
+            }
+          );
+      } else {
+        this.dialogRef.close({ password: password });
+      }
+    }
+  }
+
+  onInputChange(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    input.value = input.value.replace(/[^0-9]/g, '');
   }
 }
